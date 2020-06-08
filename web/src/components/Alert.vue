a8c5f6ee7b813daf88ea1960bb40a691bafb8778
Jorge Galvis <jorgeg@hourly.app>
2020-06-07 19:07:29 -0500

    Add report page.

---
+++ web/src/components/Alert.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="alert alert-danger" role="alert">
+     {{ message }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Alert',
+  props: ['message'],
+};
+</script>
--- web/src/components/Report.vue
+++ web/src/components/Report.vue
@@ -1,16 +1,68 @@
 <template>
-  <div>
-    <p>{{ msg }}</p>
+  <div class='container'>
+    <h3>Report </h3>
+    <div class="input-group">
+      <input v-model="period" placeholder="Type a period">
+      <button type="button" class="btn btn-primary" v-on:click="getReport"> Search </button>
+    </div>
+    <br>
+    <table class="table">
+      <thead>
+        <tr>
+          <th scope="col">Date</th>
+          <th scope="col">Sys</th>
+          <th scope="col">Dia</th>
+          <th scope="col">Pul</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="measurement in report.values" :key="measurement.date"
+          v-bind:class="{'table-danger': !measurement.ok}">
+          <th scope="row" >{{ measurement.date }}</th>
+          <td>{{ measurement.sys }}</td>
+          <td>{{ measurement.dia }}</td>
+          <td>{{ measurement.pul }}</td>
+        </tr>
+      </tbody>
+    </table>
+    <alert :message=errorMessage v-if="showMessage"></alert>
   </div>
 </template>
 
 <script>
+import axios from 'axios';
+import Alert from './Alert.vue';
+
 export default {
   name: 'Report',
   data() {
     return {
-      msg: 'The list of measurements',
+      report: '',
+      period: '',
+      showMessage: false,
+      errorMessage: '',
     };
+  },
+  components: {
+    alert: Alert,
+  },
+  methods: {
+    getReport() {
+      const { VUE_APP_API_HOST } = process.env;
+      const path = `${VUE_APP_API_HOST}/v1/report/${this.period}`;
+      axios.get(path)
+        .then((res) => {
+          this.report = res.data;
+          this.showMessage = false;
+          this.errorMessage = '';
+        })
+        .catch((error) => {
+          console.error(error);
+          this.report = {};
+          this.showMessage = true;
+          this.errorMessage = error.response.data.message;
+        });
+    },
   },
 };
 </script>
