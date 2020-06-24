5cb73c6b23b4fd9765516fd7e285427dd72ecddf
Jorge Galvis <jorlugaqui@gmail.com>
2020-06-24 11:35:11 -0500

    Use shortcut v-bind form in Alert.

--- web/src/components/Alert.vue
+++ web/src/components/Alert.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="alert"
-    v-bind:class="{'alert-danger': type === 'error', 'alert-success': type === 'ok'}" role="alert">
+    :class="{'alert-danger': type === 'error', 'alert-success': type === 'ok'}" role="alert">
      {{ message }}
   </div>
 </template>
